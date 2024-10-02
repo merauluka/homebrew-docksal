4bb6c3aa6d174bdc090624e65d397d4a62517a0b
Melissa Bent <mbent@redhat.com>
2024-10-02 10:44:58 -0600

    Update docksal.rb with latest docksal version

--- Formula/docksal.rb
+++ Formula/docksal.rb
@@ -1,7 +1,7 @@
 class Docksal < Formula
   desc "Docker powered web development environments for macOS, Windows & Linux"
   homepage "https://docksal.io"
-  url "https://github.com/docksal/docksal/archive/v1.14.1.tar.gz"
+  url "https://github.com/docksal/docksal/archive/v1.19.0.tar.gz"
   sha256 "0051a447417d5413efdcb650c387b40cc31b5b4416bdf95e19c6fe0fe3784c9d"
   license "MIT"
 
